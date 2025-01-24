12c6e9afe449d8030cb113bd6e35ce502e074bae
Abner Barreto <abnerlc4@gmail.com>
2025-01-24 00:51:18 -0300

    Crie um array para armazenar os nomes

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigo = []
+
+function adicionaramigo(){
+    let campo = document.querySelector("Digite o nome dos seus amigos");
+    if (campo = null){
+        alert("Por favor, insira um nome.");
+    }
+}
